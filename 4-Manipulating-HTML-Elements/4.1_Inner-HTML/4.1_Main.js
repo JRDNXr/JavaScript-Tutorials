f134e82c1f83e53ff172556c8334434f8bceb2ec
JRDNXr <jrdn_dsouza@hotmail.com>
2022-05-23 11:57:41 -0400

    Commit 1.2 - Adding in tutorials

---
+++ 4-Manipulating-HTML-Elements/4.1_Inner-HTML/4.1_Main.js
@@ -0,0 +1,7 @@
+
+// Adding Text to an HTML Element
+document.getElementById("answer1").innerHTML = 9;
+
+
+// Calculating and Then Adding Output (Text) to an HTML Element
+document.getElementById("answer2").innerHTML = 8 * 9;
---
+++ 4-Manipulating-HTML-Elements/4.2_Document.write/4.2_Main.js
@@ -0,0 +1,5 @@
+
+// This is typically used for testing purposes. However, document.write can overwrite (delete) all existing HTML on a webpage.
+function buttonClicked() {
+  document.write(3 + 3);
+}
