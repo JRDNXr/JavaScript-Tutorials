61e27383e10596335e5d0381fdfe05897a02c4d6
JRDNXr <jrdn_dsouza@hotmail.com>
2022-05-23 21:01:27 -0400

    Commit 1.4
    - Added more tutorials
    - Removed unnecessary files

---
+++ 1-Introduction/1.2_Declaring-and-Initializing-Variables.js
@@ -0,0 +1,32 @@
+
+// Declare variables by using the "let" statement.
+let x = 3;
+let y = 5;
+
+// You can also declare variables using the "var" statement.
+var z = 10;
+
+// You can declare constant variables. These variables are unchangeable once initialized.
+// It's best practice to use CAPITALS when creating a const variable name.
+const PIE = 3.14;
+
+// This will generate an error.
+// PIE = 100;
+
+console.log(x);
+console.log(y);
+console.log(PIE);
+
+
+// You can initialize numbers and text into variables.
+// Strings must be wrapped in quotes. You can use single quotes, double quotes, and even back quotes. Examples are shown below in the order mentioned.
+// You must start and end with the same punctuation marks.
+let firstName = 'JRDN';
+let lastName = "DSouza";
+let middleName = `Christopher Matthew`;
+
+
+// Numbers do not require quotes, and can be written as is.
+// This applies to whole numbers and decimal numbers.
+let wholeNum = 2;
+let decimalNum = 5.5;
