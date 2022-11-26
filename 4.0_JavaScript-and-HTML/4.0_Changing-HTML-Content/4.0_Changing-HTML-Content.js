32441edebddd0ec4ff1beec4bb51596cd859ab39
JRDNXr <jrdn_dsouza@hotmail.com>
2022-11-25 20:58:34 -0500

    Commit 1.0
    - Uploading initial batch of JavaScript tutorials

---
+++ 4.0_JavaScript-and-HTML/4.0_Changing-HTML-Content/4.0_Changing-HTML-Content.js
@@ -0,0 +1,15 @@
+document.getElementById("JavaScript-embed-text").innerHTML = "This sentence is not embedded from HTML, but instead by JavaScript!";
+
+function changeText() {
+    document.getElementById("changeThis").innerHTML = "The entire universe is constantly expanding!";
+}
+
+document.getElementById("JavaScript-embed-picture").src = "/Users/JRDN/Documents/Coding/JavaScript/4.0_Learning/0.0_Pictures/Cartoon-Sun.png"
+
+function changePicToCar() {
+    document.getElementById("Car-Plane-Picture").src = "/Users/JRDN/Documents/Coding/JavaScript/4.0_Learning/0.0_Pictures/Cartoon-Car.png"
+}
+
+function changePicToPlane() {
+    document.getElementById("Car-Plane-Picture").src = "/Users/JRDN/Documents/Coding/JavaScript/4.0_Learning/0.0_Pictures/Cartoon-Plane.png"
+}
